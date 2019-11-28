ee68df39538f85fd192fd0b93a7b6fde0e1427a2
loi.ryan18@gmail.com <loi.ryan18@gmail.com>
2019-11-27 18:07:55 -0700

    Login dialog code for formatting data and then passing it to the networkmanager to make a post req

--- logindialog.cpp
+++ logindialog.cpp
@@ -1,5 +1,6 @@
 #include "logindialog.h"
 #include "ui_logindialog.h"
+#include "networkmanager.h"
 
 #include <QOAuth2AuthorizationCodeFlow>
 
@@ -18,7 +19,15 @@ LoginDialog::~LoginDialog()
 void LoginDialog::on_signInButton_clicked()
 {
     // PUT LOGIN STUFF HERE
+    const QString username = ui->emailLineEdit->text();
+    const QString password = ui->passwordLineEdit->text();
+
     qDebug() << "sign in button clicked";
-    qDebug() << ui->emailLineEdit->text();
-    qDebug() << ui->passwordLineEdit->text();
+    qDebug() << username;
+    qDebug() << password;
+
+    QString data = QString("{\"username\":\"%1\",\"password\":\"%2\"}").arg(username, password);
+
+    NetworkManager* nMgr = new NetworkManager;
+    nMgr->post("http://localhost:8080/users/", data.toUtf8()); // Needs to be changed to https
 }
